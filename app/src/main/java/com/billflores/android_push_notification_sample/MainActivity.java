18baf77af704db5ef9e7eb5eb93bfbb2f7edb146
bill flores <bilizen1@hotmail.com>
2017-09-22 12:35:53 -0500

    commit my project

---
+++ app/src/main/java/com/billflores/android_push_notification_sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billflores.android_push_notification_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
